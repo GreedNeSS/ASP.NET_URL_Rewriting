4a31dbe02c2ce13baac5c453ce52fdbc9b4c7a25
GreedNeSS <ruslan199126@gmail.com>
2022-06-16 23:08:41 +0300

    Delete debug comments

--- IIS/Program.cs
+++ IIS/Program.cs
@@ -15,7 +15,5 @@ if (env is not null)
 app.MapGet("/home/index", () => "Home Index.");
 app.MapGet("/main/user", async (HttpContext context) =>
     await context.Response.WriteAsync($"Name: {context.Request.Query["name"]}; Id: {context.Request.Query["id"]}"));
-//app.MapGet("/main/user/{id}/{name}", async (HttpContext context) =>
-//    await context.Response.WriteAsync("vew"));
 
 app.Run();
