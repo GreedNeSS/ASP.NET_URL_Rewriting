b3e1560841c01775024a2e56723730f2b7291cd0
GreedNeSS <ruslan199126@gmail.com>
2022-06-16 23:07:57 +0300

    Added IIS example.

---
+++ IIS/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Rewrite;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+IHostEnvironment? env = app.Services.GetService<IHostEnvironment>();
+
+if (env is not null)
+{
+    var options = new RewriteOptions()
+        .AddIISUrlRewrite(env.ContentRootFileProvider, "Urlrewrite.xml");
+    app.UseRewriter(options);
+}
+
+app.MapGet("/home/index", () => "Home Index.");
+app.MapGet("/main/user", async (HttpContext context) =>
+    await context.Response.WriteAsync($"Name: {context.Request.Query["name"]}; Id: {context.Request.Query["id"]}"));
+//app.MapGet("/main/user/{id}/{name}", async (HttpContext context) =>
+//    await context.Response.WriteAsync("vew"));
+
+app.Run();
